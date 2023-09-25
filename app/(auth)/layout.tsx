49989fab5f3afecebf3cc769337ab68df8f5db5c
Dishant Pal <dishantmrt02@gmail.com>
2023-09-25 12:17:22 +0530

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,29 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+
+import '../globals.css';
+
+export const metadata = {
+    title: 'Threads',
+    description: 'A Next.js 13 Meta Threads Application'
+}
+
+const inter = Inter({ subsets: ["latin"] })
+
+export default function RootLayout({
+    children
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <ClerkProvider>
+            <html lang="en">
+                <body className={`${inter.className} bg-dark-1`}>
+                    <div className="w-full flex justify-center items-center min-h-screen ">
+                        {children}
+                    </div>
+                </body>
+            </html>
+        </ClerkProvider>
+    )
+}
---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,9 @@
+const Page = async () => {
+    return (
+        <search>
+            <h1 className="head-text mb-10">Communities</h1>
+        </search>
+    )
+}
+
+export default Page;
